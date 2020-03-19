58be78c274e20884520c1e3a13027ce551284f66
Holden Parker <holdenprkr@gmail.com>
2020-03-19 10:43:24 -0500

    finished exercise

---
+++ SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿--SELECT * FROM Genre
+
+--INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Green Day', 1986)
+
+--INSERT INTO Album (Title, ReleaseDate, AlbumLength, [Label], ArtistId) VALUES ('American Idiot', '9/20/2004', 3434, 'Reprise Records', 28)
+
+--INSERT INTO Song (Title, SongLength, ReleaseDate, ArtistId, AlbumId) VALUES ('American Idiot', 174, '9/20/2004', 28, 23)
+
+--DELETE FROM Album WHERE Id = 24
+--DELETE FROM Album WHERE Id = 25
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, ArtistId, AlbumId) VALUES ('Holiday', 492, '9/20/2004', 28, 23)
+INSERT INTO Song (Title, SongLength, ReleaseDate, ArtistId, AlbumId) VALUES ('Homecoming', 558, '9/20/2004', 28, 23)
+INSERT INTO Song (Title, SongLength, ReleaseDate, ArtistId, AlbumId) VALUES ('Wake Me Up When September Ends', 410, '9/20/2004', 28, 23)
+
